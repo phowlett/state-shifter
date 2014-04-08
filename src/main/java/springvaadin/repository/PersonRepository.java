35957906bc36677457a6c7a907265e0e152020c6
Paul Howlett <paul@scalar.is>
2014-04-08 22:57:17 +0100

    Annotation based repository REST resources

--- src/main/java/springvaadin/repository/PersonRepository.java
+++ src/main/java/springvaadin/repository/PersonRepository.java
@@ -4,9 +4,11 @@ import java.util.Collection;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import springvaadin.model.Person;
 
+@RepositoryRestResource(path="people")
 public interface PersonRepository extends JpaRepository<Person, Long>{
 	
 	Collection<Person> findByLastname(@Param("lastname") String lastname);
--- src/main/java/springvaadin/repository/PizzaRepository.java
+++ src/main/java/springvaadin/repository/PizzaRepository.java
@@ -1,9 +1,11 @@
 package springvaadin.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import springvaadin.model.Pizza;
 
+@RepositoryRestResource(path="pizza")
 public interface PizzaRepository extends JpaRepository<Pizza, Long>{
 
 }
