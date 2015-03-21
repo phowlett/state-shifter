023a27e4dfb1193e67115605841b1556fcdaf06c
Paul Howlett <paul@scalar.is>
2015-03-21 12:26:23 +0000

    Updated to Vaadin 7.4 with declarative UI

--- src/main/java/stateshifter/ui/PersonUI.java
+++ src/main/java/stateshifter/ui/PersonUI.java
@@ -1,17 +1,13 @@
 package stateshifter.ui;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.vaadin.spring.annotation.VaadinUI;
-import org.vaadin.teemu.clara.Clara;
-import org.vaadin.teemu.clara.binder.annotation.UiDataSource;
-import org.vaadin.teemu.clara.binder.annotation.UiField;
-import org.vaadin.teemu.clara.binder.annotation.UiHandler;
 
 import stateshifter.model.Person;
 import stateshifter.model.Pizza;
 import stateshifter.repository.PersonRepository;
 import stateshifter.repository.PizzaRepository;
 
+import com.vaadin.annotations.DesignRoot;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.data.Item;
@@ -21,17 +17,23 @@ import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
 import com.vaadin.data.util.BeanItem;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.AbstractSelect.ItemCaptionMode;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.declarative.Design;
 
-@VaadinUI
+@SpringUI
 @Title("Pizza")
-@Theme("reindeer")
+@Theme("valo")
 public class PersonUI extends UI {
 
 	private static final long serialVersionUID = 1L;
@@ -42,42 +44,65 @@ public class PersonUI extends UI {
 	@Autowired
 	private PizzaRepository pizzaRepository;
 		
-	@UiField("personTable")
-    private Table personTable;
-	
-	@UiField("personEditor")
-    private FormLayout personEditor;
-	
 	private BeanFieldGroup<Person> personFieldGroup;
 	private BeanItemContainer<Person> personContainer;
+	protected PersonDesign design;
+	
+	@DesignRoot
+	@SuppressWarnings("serial")
+	public class PersonDesign extends HorizontalSplitPanel {
+		VerticalLayout verticalLayout;
+		Button addButton;
+		Table personTable;
+		Button submitButton;
+		FormLayout personEditor;
+		TextField firstname;
+		TextField lastname;
+		ComboBox selectPizza;
+	}
 	
 	/* (non-Javadoc)
 	 * @see com.vaadin.ui.UI#init(com.vaadin.server.VaadinRequest)
 	 */
 	@Override
 	protected void init(VaadinRequest request) {
-		setContent(Clara.create("PersonUI.xml", this));
-		personTable.setVisibleColumns("firstname", "lastname", "pizza");
+		design = new PersonDesign();
+		Design.read(design);
+		setContent(design);		
+		initTable();
 		initEditor();
+	}
+	
+	/**
+	 * Initialise the table
+	 */
+	private void initTable() {
+		personContainer = new BeanItemContainer<>(Person.class, personRepository.findAll());
+		design.personTable.setContainerDataSource(personContainer);
+		design.personTable.setSelectable(true);
+		design.personTable.addValueChangeListener(event -> onPersonSelected(event));
+		design.personTable.setVisibleColumns("firstname", "lastname", "pizza");
+		design.addButton.addClickListener(event -> onAddPerson(event));
 	}
 
 	/**
 	 * Initialise the person editor and form fields
 	 */
 	private void initEditor() {
+		// bind text fields for name
 		personFieldGroup = new BeanFieldGroup<Person>(Person.class);
-		// Create and bind text fields for name
-		personEditor.addComponent(personFieldGroup.buildAndBind("First Name", "firstname"), 0);
-		personEditor.addComponent(personFieldGroup.buildAndBind("Last Name", "lastname"), 1);
+		personFieldGroup.bind(design.firstname, "firstname");
+		personFieldGroup.bind(design.lastname, "lastname");
 		
 		// Combo box for pizza selection, backed by pizza repository
-		ComboBox selectPizza = new ComboBox("Pizza", new BeanItemContainer<>(Pizza.class, pizzaRepository.findAll()));
-		selectPizza.setItemCaptionMode(ItemCaptionMode.PROPERTY);
-		selectPizza.setItemCaptionPropertyId("description");
-		personFieldGroup.bind(selectPizza, "pizza");
-		personEditor.addComponent(selectPizza, 2);
+		design.selectPizza.setContainerDataSource(new BeanItemContainer<>(Pizza.class, pizzaRepository.findAll()));
+		design.selectPizza.setItemCaptionMode(ItemCaptionMode.PROPERTY);
+		design.selectPizza.setItemCaptionPropertyId("description");
+		personFieldGroup.bind(design.selectPizza, "pizza");
+		
 		// Buffered mode, so person will not be updated until submit and commit
         personFieldGroup.setBuffered(true);
+		design.submitButton.addClickListener(event -> onSubmit(event));
 	}
 	
 	/**
@@ -94,28 +119,18 @@ public class PersonUI extends UI {
 	 */
 	private void editPerson(Item personItem) {
 		personFieldGroup.setItemDataSource(personItem);
-		personEditor.setVisible(personItem != null);
+		design.personEditor.setVisible(personItem != null);
 	}
 	
-	/**
-	 * Datasource for the table of people
-	 * @return
-	 */
-	@UiDataSource("personTable")
-	public BeanItemContainer<Person> getPersonDataSource() {
-		personContainer = new BeanItemContainer<>(Person.class, personRepository.findAll()); 
-		return personContainer;
-	}
 	
 	/**
 	 * Show the editor when person is selected in the table
 	 * @param event
 	 */
-	@UiHandler("personTable")
-    public void onPersonSelected(ValueChangeEvent event) {
-        Object personId = personTable.getValue();
+	public void onPersonSelected(ValueChangeEvent event) {
+        Object personId = design.personTable.getValue();
         if (personId != null) {
-        	editPerson(personTable.getItem(personId));
+        	editPerson(design.personTable.getItem(personId));
         }        
     }
 	
@@ -123,8 +138,7 @@ public class PersonUI extends UI {
 	 * Add a new person and show the editor when the add button is clicked
 	 * @param event
 	 */
-	@UiHandler("addButton")
-    public void onAddPerson(ClickEvent event) {
+	public void onAddPerson(ClickEvent event) {
         BeanItem<Person> personBean = new BeanItem<Person>(new Person("New", "Person"));
         editPerson(personBean);        
     }
@@ -133,7 +147,6 @@ public class PersonUI extends UI {
 	 * Save the person on editor form submission
 	 * @param event
 	 */
-	@UiHandler("submitButton")
 	public void onSubmit(ClickEvent event) {
 		if (!personFieldGroup.isValid()) {
 			Notification.show("Errors in form");
